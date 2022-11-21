2cd9f7e35855f21d677975c4ee4730eecc2a1650
Victoria <104544006+victoriagiler@users.noreply.github.com>
2022-11-20 22:52:06 -0500

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     GrocerySearchComponent,
-    ListDataComponent
+    
   ],
   imports: [
     BrowserModule,
